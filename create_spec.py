ac822e4dc3437f733b81cb637fb0abbe102ef945
Hongyu Li <lhy88562189@gmail.com>
2016-09-26 16:09:06 +0800

    Minor modification

--- create_spec.py
+++ create_spec.py
@@ -17,18 +17,21 @@ def out_put_spec(wave,flux,error,flag,i,j,folder='spec'):
 if __name__=='__main__':
   parser = OptionParser()
   parser.add_option('-f', action='store',type='string' ,dest='fname',default=None,help='filename')
+  parser.add_option('-z', action='store',type='float' ,dest='z',default=0.0,help='redshift')
   (options, args) = parser.parse_args()
   lhy=read_DRP(options.fname)
+  
   folder = '%s_spec'%lhy.PLATEIFU
   os.system('mkdir -p %s'%folder)
-  nflux = lhy.deredden()
+  lhy.deRedden()
+  lhy.deRedshift(z=options.z)
   ff = open('%s/speclist.dat'%folder,'w')
   for i in range(lhy.good_spaxel.shape[0]):
     for j in range(lhy.good_spaxel.shape[1]):
       if lhy.good_spaxel[i,j]:
         wave = lhy.wave
-        flux = nflux[:,i,j]
-        error = np.sqrt(1.0/lhy.ivar)[:,i,j]
+        flux = lhy.flux[:,i,j]
+        error = np.sqrt(1.0/lhy.ivar[:,i,j])
         good = (lhy.mask[:,i,j] == 0) * (flux > 0.0) * (error > 0.0)
         mask = np.zeros(flux.shape,dtype='int')
         mask[~good] = 2
