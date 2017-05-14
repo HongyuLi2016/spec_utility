9480c47b44001fe89bdbcf528e04773ed93327e2
Hongyu Li on zen <lhy88562189@gmail.com>
2017-05-14 22:47:18 +0800

    Minor bug fixed 	modified:   ppxf/sps-pbs_ppxf.sh 	modified:   starlight/sps-dump_starlight.py     PPXFPATH

--- ppxf/sps-pbs_ppxf.sh
+++ ppxf/sps-pbs_ppxf.sh
@@ -9,7 +9,7 @@ printf '#!/bin/bash
 #PBS -l nodes=1:ppn=1
 cd %s
 /share/data/D/lhy/manga/SPS/mpl5/make_spec.py %s 
-run_ppxf.py %s
-ssp_maps.py %s -f dump
-ssp_rebin_maps.py %s -f dump
+sps-run_ppxf.py %s
+sps-maps.py %s -f dump
+sps-rebin_maps.py %s -f dump
 ' $1 $path/$1 $path/$1 $path $1 $1 $1 $1 >  $1/ppxf.pbs
--- starlight/sps-dump_starlight.py
+++ starlight/sps-dump_starlight.py
@@ -5,7 +5,10 @@ Convert starlgiht output file to python pickle format
 from sps_starlight_out import read
 from optparse import OptionParser
 import numpy as np
-
+import os
+PPXFPATH = os.environ.get('PPXFPATH')
+if PPXFPATH is None:
+    raise RuntimeError('Enviroment variable PPXFPAHT must be set')
 
 if __name__ == '__main__':
     parser = OptionParser()
@@ -22,10 +25,10 @@ if __name__ == '__main__':
             lhy = read('{}/out/{}'.format(args[0], fname))
             if options.plot:
                 lhy.plot(shape=(25, 6), fname='sps_{:04d}.png'.format(i),
-                         filterPath='/home/lhy/python/ppxf/data/SDSS_r_filter',
+                         filterPath='{}/data/SDSS_r_filter'.format(PPXFPATH),
                          outfolder='{}/figs_star'.format(args[0]))
             lhy.dump(shape=(25, 6), fname='sps_{:04d}.dat'.format(i),
-                     filterPath='/home/lhy/python/ppxf/data/SDSS_r_filter',
+                     filterPath='{}/data/SDSS_r_filter'.format(PPXFPATH),
                      outfolder='{}/dump_star'.format(args[0]))
         except:
             print 'Warning - dump {} fiald!'.format(fname)
